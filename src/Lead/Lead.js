bbd17a6364242e67077862e2c80b3d9c997f0be0
Tess Gronski <theresegronski@Tesss-MacBook-Air.local>
2019-11-16 21:40:00 -0800

    Add image loaders

--- src/Lead/Lead.js
+++ src/Lead/Lead.js
@@ -4,6 +4,7 @@ import './Lead.css'
 import leadership from './leadership.jpg'
 import computer from './computer.jpg'
 import { Link } from 'react-router-dom';
+import Img from 'react-image';
 
 
 export default class Lead extends Component {
@@ -22,11 +23,11 @@ export default class Lead extends Component {
             <span className='contain'>      
             <section className='leadNotes'> <p>I love studying and practicing different leadership styles. I am most passionate about feedback, career coaching, and performance management. </p></section>
             <section className='articles'>
-                <Link  to='/leadership/1' ><img src={leadership} alt='desktop'></img> <br/><b >An Actionable Guide to Managing Introverts</b></Link>
+                <Link  to='/leadership/1' ><Img src={leadership} alt='desktop'loader={<p className='articleImgLoader'></p>}></Img> <br/><b >An Actionable Guide to Managing Introverts</b></Link>
                 <p>Aug 18, 2019 ||  3 min Read</p>
             </section>
             <section className='articles'>
-                <Link to='/leadership/2' ><img src={computer} alt='computerdesk'></img> <br/><b >A Running List of Newbie Resources</b></Link>
+                <Link to='/leadership/2' ><Img src={computer} alt='computerdesk'loader={<p className='articleImgLoader'></p>}></Img> <br/><b >A Running List of Newbie Resources</b></Link>
                 <p>Nov 3, 2019 ||  3 min Read</p>
             </section>
             <p className='leadQuote'>“If you want to build a ship, don’t herd people together to collect wood and don’t assign them tasks and work, but rather teach them to long for the endless immensity of the sea.” <br/>-Antoine de Saint-Exupery</p>
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -4,19 +4,17 @@ import bigsur from './bigsur.jpeg'
 import city from './city.jpeg'
 import oceanbeach from './oceanbeach.jpeg'
 import Learn from './Learn'
+import Img from 'react-image';
+
 
 
 
 export default class Main extends React.Component {
     constructor(props){
         super(props);
-        this.state={ 
-        
-        }
     }
 
     render(){
-       
        
        return(<section className='container'>
        {/* <form className="side">
@@ -24,11 +22,12 @@ export default class Main extends React.Component {
            <p className='bio'>Welcome to my portfolio site, {this.state.name}! </p>
            
        </form> */}
+        {/* <p className='blackSquare'></p> */}
        <section className='img-banner'>
         <p>B A Y <br /> A R E A </p>
-        <span className='photo-style1'><img src={bigsur} alt='big sur'/></span>
-        <span className='photo-style2'> <img src={city} alt='sf-city-skyline'/></span>  
-        <span className='photo-style3'><img src={oceanbeach} alt='ocean beach'/></span>      
+        <span className='photo-style1'><Img src={bigsur} alt='big sur'loader={<p className='mainImgLoader'></p>}/></span>
+        <span className='photo-style2'> <Img src={city} alt='sf-city-skyline'loader={<p className='mainImgLoader'></p>}/></span>  
+        <span className='photo-style3'><Img src={oceanbeach} alt='ocean beach'loader={<p className='mainImgLoader'></p>}/></span>      
        </section>
        <Learn />
        </section>)
