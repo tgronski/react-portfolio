45a6882c543198af8615ccc3e43f49a9f740d264
Tess Gronski <theresegronski@Tesss-MacBook-Air.local>
2020-01-25 13:53:49 -0800

    Adds logo file

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import './Home.css'
 import Main from '../Main/Main'
+import About from '../About/About'
 
 export default class Home extends Component {
         constructor(props){
@@ -21,6 +22,7 @@ export default class Home extends Component {
                 <p> Welcome! <br/>  <br/>I am a new developer with a background in finance, leadership, and e-Commerce. My favorite class in college was Computer Science, and I've always gravitated towards roles that have analytical and technical challenges. Recently, I made the exciting decision to invest in a coding bootcamp, so that I can pursue my passion for technology. <br/><br/>I'm learning Javascript, jQuery, React, HTML, and CSS. I'm also learning about responsive, mobile-first design, testing with Mocha and Chai, deploying API servers with Express, SQL, and more. <br/><br/> I'm also supplementing my bootcamp experience by getting involved in the tech community. Since learning to code, I've attended a conference, and I attend weekly tech meetups.  </p>
             </section>
                <Main />
+               <About/>
            </section>
             
         )
