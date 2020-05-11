2ac8e81ea746027dae8a4822b8c4c2ce939f7c16
Tess Gronski <theresegronski@Tesss-MacBook-Air.local>
2020-05-10 22:11:58 -0700

    Removes old file from Lead

--- src/Lead/Lead.js
+++ src/Lead/Lead.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "../Engineers.css";
 import "./Lead.css";
 import leadership from "./leadership.jpg";
 import computersmall from "./computersmall.jpg";
